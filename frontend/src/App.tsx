221b6a75e80061b225aed9a4cdf433861014aac0
Jolle <joel.arantola@outlook.com>
2023-10-22 19:12:33 +0300

    remove duplicate home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import SeatingOrder from "./pages/SeatingOrder";
 import Home from "./pages/Home";
 
 function App() {
+  const [message, setMessage] = useState("/message");
+  const [text, setText] = useState("/text");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/text")
+      .then((res) => res.json())
+      .then((data) => setText(data.text));
+  }, []);
+
   return (
     <Router>
       <NavigationBar />
       <Routes>
         <Route path="seatingOrder" element={<SeatingOrder />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="home" element={<Home />}></Route>
       </Routes>
-      {Home()}
+      <header className="App-header">
+        <h1>Pakollinen 'moi maailma!'</h1>
+        {message}
+        <div className="Box">{text}</div>
+      </header>
     </Router>
   );
 }

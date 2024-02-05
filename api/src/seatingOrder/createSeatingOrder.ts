3d49a00be62ef7176168501d07335c7cd37eeec3
Jolle <joel.arantola@outlook.com>
2024-02-05 20:28:49 +0200

    maintain groups better

--- api/src/seatingOrder/createSeatingOrder.ts
+++ api/src/seatingOrder/createSeatingOrder.ts
@@ -65,29 +65,20 @@ function findClosestUnseatedNeighbors(
 	const nextCouple: string[] = [];
 	var scoresForSecondPick: number[] = [];
 	if (seatingOrder.length > 0) {
-		const scoresLeft: number[] =
-			closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 1][0])];
-		const scoresRight: number[] =
-			closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 1][1])];
-		const combinedScores: number[] = combineArrays(scoresLeft, scoresRight);
+		const combinedScores: number[] = getCombinedScores(
+			people,
+			seatingOrder,
+			closenessScores
+		);
 		const combinedScoresSorted: number[] = [...combinedScores].sort(
 			(a, b) => b - a
 		);
 		for (var i = 0; i < combinedScores.length; i++) {
-			if (
-				unSeatedPeople.includes(
-					people[combinedScores.indexOf(combinedScoresSorted[i])]
-				)
-			) {
-				nextCouple.push(
-					people[combinedScores.indexOf(combinedScoresSorted[i])]
-				);
-				unSeatedPeople.splice(
-					unSeatedPeople.indexOf(
-						people[combinedScores.indexOf(combinedScoresSorted[i])]
-					),
-					1
-				);
+			const nextCandidate: string =
+				people[combinedScores.indexOf(combinedScoresSorted[i])];
+			if (unSeatedPeople.includes(nextCandidate)) {
+				nextCouple.push(nextCandidate);
+				unSeatedPeople.splice(unSeatedPeople.indexOf(nextCandidate), 1);
 				break;
 			}
 		}
@@ -132,6 +123,36 @@ function findClosestUnseatedNeighbors(
 	seatingOrder.push(nextCouple);
 	if (seatingOrder.length > 1) {
 		swapIfNeeded(people, seatingOrder, closenessScores);
+	}
+}
+
+function getCombinedScores(
+	people: string[],
+	seatingOrder: string[][],
+	closenessScores: number[][]
+): number[] {
+	const scoresLeft: number[] =
+		closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 1][0])];
+	const scoresRight: number[] =
+		closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 1][1])];
+
+	if (seatingOrder.length < 2) {
+		return combineArrays(scoresLeft, scoresRight);
+	} else {
+		const scoreLeftDistanceTwo: number[] =
+			closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 2][0])];
+		const scoreRightDistanceTwo: number[] =
+			closenessScores[people.indexOf(seatingOrder[seatingOrder.length - 2][1])];
+
+		return combineArrays(
+			combineArrays(
+				scoresLeft,
+				scoreLeftDistanceTwo.map((score) => score * 0.25)
+			),
+			combineArrays(scoresRight, scoreRightDistanceTwo).map(
+				(score) => score * 0.25
+			)
+		);
 	}
 }
 
